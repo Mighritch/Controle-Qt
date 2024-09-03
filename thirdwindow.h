0729dd23b5a77b8ea2c75e7ad2bd871481496fe8
Mighritch <100856042+Mighritch@users.noreply.github.com>
2024-09-04 01:34:05 +0200

    Update thirdwindow.h

--- thirdwindow.h
+++ thirdwindow.h
@@ -28,6 +28,32 @@ private slots:
     void on_tableView_clicked(const QModelIndex &index);
 
 
+    void on_pushButton_5_clicked();
+
+    void on_pushButton_6_clicked();
+
+    void on_pushButton_7_clicked();
+
+    void on_pushButton_8_clicked();
+
+    void on_pushButton_9_clicked();
+
+    void on_pushButton_10_clicked();
+
+    void on_pushButton_11_clicked();
+
+    void on_pushButton_12_clicked();
+
+    void on_pushButton_13_clicked();
+
+    void on_pushButton_14_clicked();
+
+    void on_lineEdit_textChanged(const QString &arg1);
+
+    void on_lineEdit_2_textChanged(const QString &arg1);
+
+    void on_lineEdit_3_textChanged(const QString &arg1);
+
 private:
     Ui::thirdwindow *ui;
     formateur *fo;
